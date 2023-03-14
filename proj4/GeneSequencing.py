409e3ac7e628b9d448201618ed0dd8b7deba5ebe
andres <afmo2@byu.edu>
2023-03-14 17:41:37 -0600

    Working with printing string

--- proj4/GeneSequencing.py
+++ proj4/GeneSequencing.py
@@ -29,37 +29,68 @@ class GeneSequencing:
     def __init__(self):
         self.MaxCharactersToAlign = None
 
-    # This is the method called by the GUI.  _seq1_ and _seq2_ are two sequences to be aligned, _banded_ is a boolean that tells
-    # you whether you should compute a banded alignment or full alignment, and _align_length_ tells you
-    # how many base pairs to use in computing the alignment
+        # This is the method called by the GUI.  _seq1_ and _seq2_ are two sequences to be aligned, _banded_ is a boolean that tells
+        # you whether you should compute a banded alignment or full alignment, and _align_length_ tells you
+        # how many base pairs to use in computing the alignment
 
     def align(self, seq1, seq2, banded, align_length):
         self.banded = banded
         self.MaxCharactersToAlign = align_length
 
+        SEQUENCE1 = "attggcgtccgtacgtaccctttctactctcaaactcttgttagtttaaatctaatctaaactttataaacggcacttcctgtgtgtccatgcccgtgggcttggtcttgtcatagtgctgacatttgtggttccttggtttttgttctctgccagtgacgtgtccattcggcgccagcagcccacccataggttgcataatggcaaagatgggcaaatacggtctcggcttcaaatgggccccagaatttccatggatgcttccgaacgcatcggagaagttgggtagccctgagaggtcagaggaggatgggttttgcccctctgctgcgcaagaaccaaaaactaaaggaaaaactttggttaatcacgtgagggtgaattgtagccggcttccagctttggaatgctgtgttcagtctgccataatccgtgatatttttgtagatgaggatccccagaaggtggaggcctcaactatgatggcattgcagttcggtagtgccgtcttggttaagccatccaagcgcttgtctattcaggcatggactaatttgggtgtgcttcccaaaacagctgccatggggttgttcaagcgcgtctgcctgtgtaacaccagggagtgctcttgtgacgcccacgtggcctttcacctttttacggtccaacccgatggtgtatgcctgggtaatggccgttttataggctggttcgttccagtcacagccataccggagtatgcgaagcagtggttgcaaccctggtccatccttcttcgtaagggtggtaacaaagggtctgtgacatccggccacttccgccgcgctgttaccatgcctgtgtatgactttaatgtagaggatgcttgtgaggaggttcatcttaacccgaagggtaagtactcctgcaaggcgtatgctcttcttaagggctatcgcggtgttaagcccatcctgtttgtggaccagtatggttgcgactatactggatgtctcgccaagggtcttgaggactatggcgatctcaccttgagtgagatgaaggagttgttccctgtgtggcgtgactccttggatagtgaagtccttgtggcttggcacgttgatcgagatcctcgggctgctatgcgtctgcagactcttgctactgtacgttgcattgattatgtgggccaaccgaccgaggatgtggtggatggagatgtggtagtgcgtgagcctgctcatcttctcgcagccaatgccattgttaaaagactcccccgtttggtggagactatgctgtatacggattcgtccgttacagaattctgttataaaaccaagctgtgtgaatgcggttttatcacgcagtttggctatgtggattgttgtggtgacacctgcgattttcgtgggtgggttgccggcaatatgatggatggctttccatgtccagggtgtaccaaaaattatatgccctgggaattggaggcccagtcatcaggtgttataccagaaggaggtgttctattcactcagagcactgatacagtgaatcgtgagtcctttaagctctacggtcatgctgttgtgccttttggttctgctgtgtattggagcccttgcccaggtatgtggcttccagtaatttggtcttctgttaagtcatactctggtttgacttatacaggagtagttg"
+        SEQUENCE2 = "ataagagtgattggcgtccgtacgtaccctttctactctcaaactcttgttagtttaaatctaatctaaactttataaacggcacttcctgtgtgtccatgcccgtgggcttggtcttgtcatagtgctgacatttgtggttccttggtttttgttctctgccagtgacgtgtccattcggcgccagcagcccacccataggttgcataatggcaaagatgggcaaatacggtctcggcttcaaatgggccccagaatttccatggatgcttccgaacgcatcggagaagttgggtagccctgagaggtcagaggaggatgggttttgcccctctgctgcgcaagaaccaaaaactaaaggaaaaactttgattaatcacgtgagggtggattgtagccggcttccagcattggagtgctgtgttcagtctgccataatccgtgatatttttgttgatgaggatcccttgaatgtggaggcctcaactatgatggcattgcagttcggtagtgctgtcttggtcaagccatccaagcgcttgtctattcaggcatgggctaagttgggtgtgctgcctaaaactccagccatggggttgttcaagcgcttctgcctgtgtaacaccagggagtgcgtttgtgacgcccacgtggcttttcaactttttacggtccagcctgatggtgtatgcctgggcaatggccgttttataggctggtttgtgccagtcacagccataccggcgtatgcgaagcagtggttgcaaccctggtccatccttcttcgtaagggtggtaacaaaggttctgtaacatccggccatttccgccgcgctgttaccatgcctgtgtatgactttaatgtggaggatgcttgtgaggaggttcatcttaacccgaagggtaagtactcccgcaaggcgtatgctcttcttaagggctatcgcggtgttaaatccatcctattcttggaccagtatggttgtgactatactgggcgtctcgccaagggtcttgaagactatggcgattgtactttggaagagatgaaggagttgtttcctgtgtggtgtgactccttggataatgaagttgttgtggcctggcatgttgatcgggatcctcgggctgttatgcgcttgcagactcttgctactgtacgttgcattggttatgtgggccaaccgaccgaggatttggttgatggagatgtggtagtgcgtgagcctgctcatctcctagcagccaatgccatcgtca"
+
         min_x = min(len(seq1), self.MaxCharactersToAlign)
         min_y = min(len(seq2), self.MaxCharactersToAlign)
-        score = None
-        table = None
         if banded is True:
             table = self.banded_algorithm(seq1, seq2)
             min_y = table[len(table) - 1][4]
             # min_y = self.get_last_item_index(table)
             score = min_y.get('cost')
         else:
-            table = self.unrestricted_algorithm(seq2, seq1)
+            table = self.unrestricted_algorithm(SEQUENCE1, SEQUENCE2)
             score = table[min_x, min_y].get('cost')
         ###################################################################################################
         # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
+        alignments = self.create_alignment(table, SEQUENCE1, SEQUENCE2)
 
+        return {'align_cost': score, 'seqi_first100': alignments["seq1"], 'seqj_first100': alignments["seq2"]}
 
-        alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
-            len(seq1), align_length, ',BANDED' if banded else '')
-        alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
-            len(seq2), align_length, ',BANDED' if banded else '')
-        ###################################################################################################
+    def create_alignment(self, table, seq1, seq2):
+        stack = []
+        node = table[len(table) - 1, len(table[len(table) - 1]) - 1]
+        stack.append(node)
+        # Adds to the stack all the aligment
+        while node["prev"] is not None:
+            node = table[node["prev"][0], node["prev"][1]]
+            stack.append(node)
 
-        return {'align_cost': score, 'seqi_first100': alignment1, 'seqj_first100': alignment2}
+        sequence1 = ""
+        sequence2 = ""
+        index1 = 0
+        index2 = 0
+        while len(stack) != 1 or index2 == 50 or index1 == 50:
+            curNode = stack.pop()
+            if stack[-1]['cost'] - curNode['cost'] == -3 or stack[-1]['cost'] - curNode['cost'] == 1:
+                if index1 < len(seq1):
+                    sequence1 += seq1[index1]
+                if index2 < len(seq2):
+                    sequence2 += seq2[index2]
+                index1 += 1
+                index2 += 1
+            else:
+                if stack[-1]['prev'][0] == curNode['prev'][0]:
+                    if index1 < len(seq1):
+                        sequence1 += seq1[index1]
+                    sequence2 += '-'
+                    index1 += 1
+                else:
+                    sequence1 += '-'
+                    if index2 < len(seq2):
+                        sequence2 += seq2[index2]
+                    index2 += 1
+
+        return {"seq1": sequence1, "seq2": sequence2}
 
     def unrestricted_algorithm(self, seq1, seq2):
         min_x = min(len(seq1), self.MaxCharactersToAlign)
@@ -98,7 +129,8 @@ class GeneSequencing:
                 equalChar = seq2[curColumn - 1] == seq1[curRow - 1]
                 # Append new row
                 neighbours = self.calculateNeighbours(seq2, curRow, curColumn, table, nextSpot['j'])
-                table[nextSpot['i']][nextSpot['j']] = self.banded_min_distance(neighbours['left'], neighbours['upLeft'], neighbours['up'], equalChar, nextSpot)
+                table[nextSpot['i']][nextSpot['j']] = self.banded_min_distance(neighbours['left'], neighbours['upLeft'],
+                                                                               neighbours['up'], equalChar, nextSpot)
                 curColumn += 1
                 nextSpot['j'] += 1
             curRow += 1
@@ -107,8 +139,7 @@ class GeneSequencing:
                 table.append([0 for i in range(8)])
         return table
 
-
-    def calculateNeighbours(self, seq2, curRow, curColumn,  table, nextSpot):
+    def calculateNeighbours(self, seq2, curRow, curColumn, table, nextSpot):
         lower_bound = max(1, curRow - MAXINDELS)
         upper_bound = min(curRow + MAXINDELS, len(seq2))
 
@@ -122,7 +153,6 @@ class GeneSequencing:
                 return {'left': table[curRow][nextSpot - 1], 'upLeft': table[curRow - 1][nextSpot],
                         'up': table[curRow - 1][nextSpot + 1]}
 
-
         if curRow <= 4:
             return {'left': table[curRow][nextSpot - 1], 'upLeft': table[curRow - 1][nextSpot - 1],
                     'up': table[curRow - 1][nextSpot]}
@@ -135,21 +165,16 @@ class GeneSequencing:
             return {'left': table[curRow][nextSpot - 1], 'upLeft': table[curRow - 1][nextSpot],
                     'up': table[curRow - 1][nextSpot + 1]}
 
-
     def min_distance(self, dist_left, dist_upLeft, dist_up, equalChar, i, j):
-        min_dist = None
-        min_location = None
-
         if equalChar is False:
-
             min_dist = min(dist_left.get('cost'), dist_upLeft.get('cost'), dist_up.get('cost'))
 
-            if min_dist == dist_upLeft.get('cost'):
-                min_location = dist_upLeft
-                min_dist += SUB
-            elif min_dist == dist_left.get('cost'):
+            if min_dist == dist_left.get('cost'):
                 min_location = dist_left
                 min_dist += INDEL
+            elif min_dist == dist_upLeft.get('cost'):
+                min_location = dist_upLeft
+                min_dist += SUB
             else:
                 min_location = dist_up
                 min_dist += INDEL
@@ -157,13 +182,23 @@ class GeneSequencing:
             min_location = dist_upLeft
             min_dist = dist_upLeft.get('cost') - 3
 
+        # Tiebreakers
+        if min_location == dist_left:
+            if dist_upLeft.get('cost') == dist_left.get('cost') + SUB:
+                min_location = dist_upLeft
+                min_dist = dist_left.get('cost') + SUB
+            elif dist_up.get('cost') == dist_left.get('cost') + INDEL:
+                min_location = dist_up
+                min_dist = dist_left.get('cost') + INDEL
+        elif min_location == dist_up:
+            if dist_upLeft.get('cost') == dist_up.get('cost') + SUB:
+                min_location = dist_upLeft
+                min_dist = dist_up.get('cost') + SUB
+
         min_location = min_location['loc']
         return {'prev': min_location, 'cost': min_dist, 'loc': (i, j)}
 
     def banded_min_distance(self, dist_left, dist_upLeft, dist_up, equalChar, nextSpot):
-        min_dist = None
-        min_location = None
-
         if equalChar is False:
             # Check if within the band
             if dist_left != 0:
@@ -210,7 +245,6 @@ class GeneSequencing:
             table[0, j] = {'prev': None, 'cost': curValue, 'loc': (0, j)}
             curValue += INDEL
 
-
     def banded_table_initializer(self, table):
         curValue = 0
         array = []
@@ -233,7 +267,6 @@ class GeneSequencing:
                 else:
                     array.append(0)
             table.append(array)
-
 
     def get_last_item_index(self, table):
         last_item = None
