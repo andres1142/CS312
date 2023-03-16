3699be3566fe0e2c65d139a88bbeb9d92f99274b
andres <afmo2@byu.edu>
2023-03-15 18:01:47 -0600

    Aligment working for Unrestricted. Stopped working on banded alignment.

--- proj4/GeneSequencing.py
+++ proj4/GeneSequencing.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from math import inf
 
 from which_pyqt import PYQT_VER
 
@@ -37,91 +38,179 @@ class GeneSequencing:
         self.banded = banded
         self.MaxCharactersToAlign = align_length
 
-        SEQUENCE1 = "attggcgtccgtacgtaccctttctactctcaaactcttgttagtttaaatctaatctaaactttataaacggcacttcctgtgtgtccatgcccgtgggcttggtcttgtcatagtgctgacatttgtggttccttggtttttgttctctgccagtgacgtgtccattcggcgccagcagcccacccataggttgcataatggcaaagatgggcaaatacggtctcggcttcaaatgggccccagaatttccatggatgcttccgaacgcatcggagaagttgggtagccctgagaggtcagaggaggatgggttttgcccctctgctgcgcaagaaccaaaaactaaaggaaaaactttggttaatcacgtgagggtgaattgtagccggcttccagctttggaatgctgtgttcagtctgccataatccgtgatatttttgtagatgaggatccccagaaggtggaggcctcaactatgatggcattgcagttcggtagtgccgtcttggttaagccatccaagcgcttgtctattcaggcatggactaatttgggtgtgcttcccaaaacagctgccatggggttgttcaagcgcgtctgcctgtgtaacaccagggagtgctcttgtgacgcccacgtggcctttcacctttttacggtccaacccgatggtgtatgcctgggtaatggccgttttataggctggttcgttccagtcacagccataccggagtatgcgaagcagtggttgcaaccctggtccatccttcttcgtaagggtggtaacaaagggtctgtgacatccggccacttccgccgcgctgttaccatgcctgtgtatgactttaatgtagaggatgcttgtgaggaggttcatcttaacccgaagggtaagtactcctgcaaggcgtatgctcttcttaagggctatcgcggtgttaagcccatcctgtttgtggaccagtatggttgcgactatactggatgtctcgccaagggtcttgaggactatggcgatctcaccttgagtgagatgaaggagttgttccctgtgtggcgtgactccttggatagtgaagtccttgtggcttggcacgttgatcgagatcctcgggctgctatgcgtctgcagactcttgctactgtacgttgcattgattatgtgggccaaccgaccgaggatgtggtggatggagatgtggtagtgcgtgagcctgctcatcttctcgcagccaatgccattgttaaaagactcccccgtttggtggagactatgctgtatacggattcgtccgttacagaattctgttataaaaccaagctgtgtgaatgcggttttatcacgcagtttggctatgtggattgttgtggtgacacctgcgattttcgtgggtgggttgccggcaatatgatggatggctttccatgtccagggtgtaccaaaaattatatgccctgggaattggaggcccagtcatcaggtgttataccagaaggaggtgttctattcactcagagcactgatacagtgaatcgtgagtcctttaagctctacggtcatgctgttgtgccttttggttctgctgtgtattggagcccttgcccaggtatgtggcttccagtaatttggtcttctgttaagtcatactctggtttgacttatacaggagtagttg"
-        SEQUENCE2 = "ataagagtgattggcgtccgtacgtaccctttctactctcaaactcttgttagtttaaatctaatctaaactttataaacggcacttcctgtgtgtccatgcccgtgggcttggtcttgtcatagtgctgacatttgtggttccttggtttttgttctctgccagtgacgtgtccattcggcgccagcagcccacccataggttgcataatggcaaagatgggcaaatacggtctcggcttcaaatgggccccagaatttccatggatgcttccgaacgcatcggagaagttgggtagccctgagaggtcagaggaggatgggttttgcccctctgctgcgcaagaaccaaaaactaaaggaaaaactttgattaatcacgtgagggtggattgtagccggcttccagcattggagtgctgtgttcagtctgccataatccgtgatatttttgttgatgaggatcccttgaatgtggaggcctcaactatgatggcattgcagttcggtagtgctgtcttggtcaagccatccaagcgcttgtctattcaggcatgggctaagttgggtgtgctgcctaaaactccagccatggggttgttcaagcgcttctgcctgtgtaacaccagggagtgcgtttgtgacgcccacgtggcttttcaactttttacggtccagcctgatggtgtatgcctgggcaatggccgttttataggctggtttgtgccagtcacagccataccggcgtatgcgaagcagtggttgcaaccctggtccatccttcttcgtaagggtggtaacaaaggttctgtaacatccggccatttccgccgcgctgttaccatgcctgtgtatgactttaatgtggaggatgcttgtgaggaggttcatcttaacccgaagggtaagtactcccgcaaggcgtatgctcttcttaagggctatcgcggtgttaaatccatcctattcttggaccagtatggttgtgactatactgggcgtctcgccaagggtcttgaagactatggcgattgtactttggaagagatgaaggagttgtttcctgtgtggtgtgactccttggataatgaagttgttgtggcctggcatgttgatcgggatcctcgggctgttatgcgcttgcagactcttgctactgtacgttgcattggttatgtgggccaaccgaccgaggatttggttgatggagatgtggtagtgcgtgagcctgctcatctcctagcagccaatgccatcgtca"
+        min_x = min(len(seq2), self.MaxCharactersToAlign)
+        min_y = min(len(seq1), self.MaxCharactersToAlign)
 
-        min_x = min(len(seq1), self.MaxCharactersToAlign)
-        min_y = min(len(seq2), self.MaxCharactersToAlign)
         if banded is True:
             table = self.banded_algorithm(seq1, seq2)
-            min_y = table[len(table) - 1][4]
-            # min_y = self.get_last_item_index(table)
-            score = min_y.get('cost')
+            min_y = (table[len(table) - 1][4], 4) if min_x > 100 else self.get_last_item_index(table)
+            score = min_y
+            alignments = self.create_banded_alignment(table, seq1, seq2, min_y)
         else:
-            table = self.unrestricted_algorithm(SEQUENCE1, SEQUENCE2)
-            score = table[min_x, min_y].get('cost')
-        ###################################################################################################
-        # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
-        alignments = self.create_alignment(table, SEQUENCE1, SEQUENCE2)
+            table = self.unrestricted_algorithm(seq1, seq2)
+            score = table[min_x, min_y]
+            alignments = self.create_alignment(table, seq1, seq2)
 
-        return {'align_cost': score, 'seqi_first100': alignments["seq1"], 'seqj_first100': alignments["seq2"]}
+        sequence1 = alignments["seq1"][:100] if len(seq1) > 100 else alignments["seq1"]
+        sequence2 = alignments["seq2"][:100] if len(seq2) > 100 else alignments["seq2"]
 
-    def create_alignment(self, table, seq1, seq2):
-        stack = []
-        node = table[len(table) - 1, len(table[len(table) - 1]) - 1]
-        stack.append(node)
-        # Adds to the stack all the aligment
-        while node["prev"] is not None:
-            node = table[node["prev"][0], node["prev"][1]]
-            stack.append(node)
+        return {'align_cost': score[0], 'seqi_first100': sequence1,
+                'seqj_first100': sequence2}
 
-        sequence1 = ""
-        sequence2 = ""
-        index1 = 0
-        index2 = 0
-        while len(stack) != 1 or index2 == 50 or index1 == 50:
-            curNode = stack.pop()
-            if stack[-1]['cost'] - curNode['cost'] == -3 or stack[-1]['cost'] - curNode['cost'] == 1:
-                if index1 < len(seq1):
-                    sequence1 += seq1[index1]
-                if index2 < len(seq2):
-                    sequence2 += seq2[index2]
-                index1 += 1
-                index2 += 1
+    def create_banded_alignment(self, table, seq1, seq2, lastColumn):
+
+        if len(seq1) < self.MaxCharactersToAlign or len(seq2) < self.MaxCharactersToAlign:
+            return {"seq1": "No Alignment Possible.", "seq2": "No Alignment Possible."}
+        row = table.shape[0] - 1
+        col = lastColumn[1]
+
+        sequence1 = " "
+        sequence2 = " "
+        node = table[row][col]
+        index = self.MaxCharactersToAlign
+        while row != 0:
+            equalChar = seq2[row - 1] == seq1[index]
+
+            if row > 4:
+                if col != 0 and col != 7:
+                    up_cell = (table[row - 1, col + 1], [row - 1, col + 1])
+                    left_cell = (table[row, col - 1], [row, col - 1])
+                    upLeft_cell = (table[row - 1, col], [row - 1, col])
+
+                # Work with lower bound
+                elif col == 0:
+                    up_cell = (table[row - 1, col + 1], [row - 1, col + 1])
+                    left_cell = (float(inf), float(inf))
+                    upLeft_cell = (table[row - 1, col], [row - 1, col])
+                # Work with upper bound
+                elif col == 7:
+                    up_cell = (float(inf), float(inf))
+                    left_cell = (table[row, col - 1], [row, col - 1])
+                    upLeft_cell = (table[row - 1, col], [row - 1, col])
+
+                min_value = min(up_cell[0] + INDEL, left_cell[0] + INDEL,
+                                (upLeft_cell[0] + MATCH) if equalChar is True else (
+                                            upLeft_cell[0] + SUB))
+
+
+                if min_value == left_cell[0] + INDEL:
+                    sequence1 = seq1[index - 1] + sequence1
+                    sequence2 = "-" + sequence2
+                    index -= 1
+                    col = left_cell[1][1]
+                    continue
+
+                if min_value == up_cell[0] + INDEL:
+                    sequence1 = "-" + sequence1
+                    sequence2 = seq2[row - 1] + sequence2
+                    row -= 1
+                    col = up_cell[1][1]
+                    continue
+
+                if min_value == upLeft_cell[0] + MATCH or min_value == upLeft_cell[0] + SUB:
+                    sequence1 = seq1[index - 1] + sequence1
+                    sequence2 = seq2[row - 1] + sequence2
+                    col = upLeft_cell[1][1]
+                    row -= 1
+                    index -= 1
+                    continue
             else:
-                if stack[-1]['prev'][0] == curNode['prev'][0]:
-                    if index1 < len(seq1):
-                        sequence1 += seq1[index1]
-                    sequence2 += '-'
-                    index1 += 1
-                else:
-                    sequence1 += '-'
-                    if index2 < len(seq2):
-                        sequence2 += seq2[index2]
-                    index2 += 1
+
+                up_cell = (table[row - 1,col], [row - 1, col])
+                left_cell = (table[row, col - 1], [row, col - 1])
+                upLeft_cell = (table[row - 1, col - 1], [row - 1, col - 1])
+
+                min_value = min(up_cell[0] + INDEL, left_cell[0] + INDEL,
+                                (upLeft_cell[0] + MATCH) if equalChar is True else (
+                                        upLeft_cell[0] + SUB))
+
+                if min_value == left_cell[0] + INDEL:
+                    sequence1 = seq1[index - 1] + sequence1
+                    sequence2 = "-" + sequence2
+                    index -= 1
+                    col = left_cell[1][1]
+                    continue
+
+                if min_value == up_cell[0] + INDEL:
+                    sequence1 = "-" + sequence1
+                    sequence2 = seq2[row - 1] + sequence2
+                    row -= 1
+                    col = up_cell[1][1]
+                    continue
+
+                if min_value == upLeft_cell[0] + MATCH or min_value == upLeft_cell[0] + SUB:
+                    sequence1 = seq1[index - 1] + sequence1
+                    sequence2 = seq2[row - 1] + sequence2
+                    col = upLeft_cell[1][1]
+                    row -= 1
+                    index -= 1
+                    continue
+
+
 
         return {"seq1": sequence1, "seq2": sequence2}
 
-    def unrestricted_algorithm(self, seq1, seq2):
-        min_x = min(len(seq1), self.MaxCharactersToAlign)
-        min_y = min(len(seq2), self.MaxCharactersToAlign)
+    def create_alignment(self, table, seq1, seq2):
+        row = table.shape[0] - 1
+        col = table.shape[1] - 1
 
-        table = np.zeros((min_y + 1, min_x + 1), dtype=object)
+        sequence1 = ""
+        sequence2 = ""
+
+        while row != 0 and col != 0:
+            equalChar = seq2[row - 1] == seq1[col - 1]
+            up_cell = table[row - 1, col]
+            left_cell = table[row, col - 1]
+            upLeft_cell = table[row - 1, col - 1]
+            min_value = min(up_cell + INDEL, left_cell + INDEL,
+                            (table[row - 1, col - 1] + MATCH) if equalChar is True else (table[row - 1, col - 1] + SUB))
+
+            if min_value == left_cell + INDEL:
+                sequence1 = seq1[col - 1] + sequence1
+                sequence2 = "-" + sequence2
+                col -= 1
+                continue
+
+            if min_value == up_cell + INDEL:
+                sequence1 = "-" + sequence1
+                sequence2 = seq2[row - 1] + sequence2
+                row -= 1
+                continue
+
+            if min_value == upLeft_cell + MATCH or min_value == upLeft_cell + SUB:
+                sequence1 = seq1[col - 1] + sequence1
+                sequence2 = seq2[row - 1] + sequence2
+                row -= 1
+                col -= 1
+                continue
+
+        return {"seq1": sequence1, "seq2": sequence2}
+
+
+
+
+    def unrestricted_algorithm(self, seq1, seq2):
+        min_y = min(len(seq1), self.MaxCharactersToAlign)
+        min_x = min(len(seq2), self.MaxCharactersToAlign)
+
+        table = np.zeros((min_x + 1, min_y + 1), dtype=int)
         self.table_initializer(table)
 
-        for i in range(1, len(seq2) + 1):
-            for j in range(1, len(seq1) + 1):
-                equalChar = seq1[j - 1] == seq2[i - 1]
-                table[i, j] = self.min_distance(table[i, j - 1], table[i - 1, j - 1], table[i - 1, j], equalChar, i, j)
-
-                # Check if align_length is exceeded
-                if j == self.MaxCharactersToAlign:
-                    break
-            if i == self.MaxCharactersToAlign:
-                break
+        for i in range(1, min_y + 1):
+            for j in range(1, min_x + 1):
+                equalChar = seq2[j - 1] == seq1[i - 1]
+                table[j, i] = self.min_distance(table[j, i - 1], table[j - 1, i - 1], table[j - 1, i], equalChar)
         return table
 
     def banded_algorithm(self, seq1, seq2):
         min_y = min(len(seq1), self.MaxCharactersToAlign)
         # Create the first cases
-        table = []
-        self.banded_table_initializer(table)
+        table = self.banded_table_initializer()
 
         # Get bounds
         curRow = 1
         while curRow <= min_y:
             nextSpot = {"i": curRow, "j": 1}
-
             curColumn = max(1, curRow - MAXINDELS)
             upper_bound = min(curRow + MAXINDELS, len(seq2))
 
@@ -130,13 +219,13 @@ class GeneSequencing:
                 # Append new row
                 neighbours = self.calculateNeighbours(seq2, curRow, curColumn, table, nextSpot['j'])
                 table[nextSpot['i']][nextSpot['j']] = self.banded_min_distance(neighbours['left'], neighbours['upLeft'],
-                                                                               neighbours['up'], equalChar, nextSpot)
+                                                                               neighbours['up'], equalChar)
                 curColumn += 1
                 nextSpot['j'] += 1
             curRow += 1
 
             if min_y >= len(table) and curRow >= 4:
-                table.append([0 for i in range(8)])
+                table = np.vstack([table, np.zeros(table.shape[1], dtype=int)])
         return table
 
     def calculateNeighbours(self, seq2, curRow, curColumn, table, nextSpot):
@@ -165,115 +254,72 @@ class GeneSequencing:
             return {'left': table[curRow][nextSpot - 1], 'upLeft': table[curRow - 1][nextSpot],
                     'up': table[curRow - 1][nextSpot + 1]}
 
-    def min_distance(self, dist_left, dist_upLeft, dist_up, equalChar, i, j):
-        if equalChar is False:
-            min_dist = min(dist_left.get('cost'), dist_upLeft.get('cost'), dist_up.get('cost'))
 
-            if min_dist == dist_left.get('cost'):
-                min_location = dist_left
-                min_dist += INDEL
-            elif min_dist == dist_upLeft.get('cost'):
-                min_location = dist_upLeft
-                min_dist += SUB
-            else:
-                min_location = dist_up
-                min_dist += INDEL
-        else:
-            min_location = dist_upLeft
-            min_dist = dist_upLeft.get('cost') - 3
-
-        # Tiebreakers
-        if min_location == dist_left:
-            if dist_upLeft.get('cost') == dist_left.get('cost') + SUB:
-                min_location = dist_upLeft
-                min_dist = dist_left.get('cost') + SUB
-            elif dist_up.get('cost') == dist_left.get('cost') + INDEL:
-                min_location = dist_up
-                min_dist = dist_left.get('cost') + INDEL
-        elif min_location == dist_up:
-            if dist_upLeft.get('cost') == dist_up.get('cost') + SUB:
-                min_location = dist_upLeft
-                min_dist = dist_up.get('cost') + SUB
-
-        min_location = min_location['loc']
-        return {'prev': min_location, 'cost': min_dist, 'loc': (i, j)}
-
-    def banded_min_distance(self, dist_left, dist_upLeft, dist_up, equalChar, nextSpot):
+    def banded_min_distance(self, dist_left, dist_upLeft, dist_up, equalChar):
         if equalChar is False:
             # Check if within the band
             if dist_left != 0:
-                left_dist = dist_left.get('cost')
+                left_dist = dist_left
             else:
-                left_dist = float('inf')
+                left_dist = 0
             if dist_upLeft != 0:
-                upleft_dist = dist_upLeft.get('cost')
+                upleft_dist = dist_upLeft
             else:
-                upleft_dist = float('inf')
+                upleft_dist = 0
             if dist_up != 0:
-                up_dist = dist_up.get('cost')
+                up_dist = dist_up
             else:
-                up_dist = float('inf')
+                up_dist = 0
 
             min_dist = min(left_dist + INDEL, upleft_dist + SUB, up_dist + INDEL)
-            upLeftAdded = False
-            if min_dist == upleft_dist + SUB:
-                min_location = dist_upLeft
-                upLeftAdded = True
-
-            if min_dist == left_dist + INDEL and not upLeftAdded:
-                min_location = dist_left
-
-            if min_dist == up_dist + INDEL and not upLeftAdded:
-                min_location = dist_up
         else:
-            min_location = dist_upLeft
-            min_dist = dist_upLeft.get('cost') - 3
+            min_dist = dist_upLeft + MATCH
 
-        min_location = min_location['loc']
-        return {'prev': min_location, 'cost': min_dist, 'loc': (nextSpot['i'], nextSpot['j'])}
+        return min_dist
+
+    def min_distance(self, dist_left, dist_upLeft, dist_up, equalChar):
+        if equalChar is False:
+            return min(dist_left + INDEL, dist_upLeft + SUB, dist_up + INDEL)
+        else:
+            return min(dist_left + INDEL, dist_upLeft + MATCH, dist_up + INDEL)
 
     def table_initializer(self, table):
         # Populates the first column.
         curValue = 0
         for i in range(len(table)):
-            table[i, 0] = {'prev': None, 'cost': curValue, 'loc': (i, 0)}
+            table[i, 0] = curValue
             curValue += INDEL
 
         curValue = 0
         # Populates the first row
         for j in range(len(table[0])):
-            table[0, j] = {'prev': None, 'cost': curValue, 'loc': (0, j)}
+            table[0, j] = curValue
             curValue += INDEL
 
-    def banded_table_initializer(self, table):
+    def banded_table_initializer(self):
+        table = np.empty((0, 8), dtype=int)
         curValue = 0
-        array = []
+        array = np.zeros(8, dtype=int)
+
         for i in range(0, 8):
             if i < MAXINDELS + 1:
-                array.append({'prev': None, 'cost': curValue, 'loc': (i, 0)})
+                array[i] = curValue
                 curValue += INDEL
-            else:
-                array.append(0)
 
-        table.append(array)
+        table = np.vstack([table, array])
 
         curValue = 5
-        for j in range(1, MAXINDELS + 1):
-            array = []
-            for k in range(0, 8):
-                if k == 0:
-                    array.append({'prev': None, 'cost': curValue, 'loc': (0, j)})
-                    curValue += INDEL
-                else:
-                    array.append(0)
-            table.append(array)
+        for i in range(3):
+            array = np.zeros(8, dtype=int)
+            array[0] = curValue
+            table = np.vstack([table, array])
+            curValue += 5
+        return table
 
     def get_last_item_index(self, table):
         last_item = None
         for i in range(len(table[len(table) - 1])):
             if table[len(table) - 1][i] == 0 and i != 0:
-                return last_item
+                return (last_item, i - 1)
             else:
                 last_item = table[len(table) - 1][i]
-
-        return last_item
